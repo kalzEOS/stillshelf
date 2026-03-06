ff8b7c81294674ac1f3be1a2792968854f7b753c
kalzEOS <49320606+kalzEOS@users.noreply.github.com>
2026-03-05 23:16:19 -0500

    chore(release): bump version to 0.1.0-rc1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.stillshelf.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 14
-        versionName = "0.1.0-rc1.6"
+        versionCode = 15
+        versionName = "0.1.0-rc1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

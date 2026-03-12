d29a200be59a52efa8203d8e734bb4aad6d78de3
kalzEOS <49320606+kalzEOS@users.noreply.github.com>
2026-03-12 14:41:27 -0400

    Bump beta version to 0.1.9-beta.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.stillshelf.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 27
-        versionName = "0.1.9-beta.1"
+        versionCode = 28
+        versionName = "0.1.9-beta.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
